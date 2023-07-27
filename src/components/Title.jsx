b36d7c00ec272da55bb8a7f412e21bc145328b3d
Marquillos-99 <marckgit@gmail.com>
2023-07-27 00:30:31 -0600

    first

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+export default function title(){
+  return(
+  <h1
+    className=" text-white font-black text-6xl md:text-8xl text-center mb-10 px-4"
+  >
+    Русский
+    <span class="block text-[36px] md:text-[58px]">семья</span>
+  </h1>
+  )
+}
